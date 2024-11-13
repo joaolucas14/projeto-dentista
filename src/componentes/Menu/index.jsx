989b85efb37bae15f4b4e6956dc3b2a7e8b1629c
joaolucas14 <jlparceiro10@gmail.com>
2024-11-13 18:22:09 -0300

    adicionand o bootstrap e iniciando o menu

---
+++ src/componentes/Menu/index.jsx
@@ -0,0 +1,42 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "./Menu.module.css";
+import whatsapp from "./whatsapp.png";
+import instagram from "./instagram.png";
+import facebook from "./facebook.png";
+import logo from "./logo.png";
+function Menu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className={styles.menu}>
+      <Container>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Páginas</Nav.Link>
+            <Nav.Link href="#">Procedimentos</Nav.Link>
+            <Nav.Link href="#">Sobre</Nav.Link>
+            <Nav.Link href="#">Depoimentos</Nav.Link>
+            <Nav.Link href="#">Contato</Nav.Link>
+            <Nav.Link href="#">Mapa</Nav.Link>
+          </Nav>
+          <Nav>
+            <div className={styles.contatos}>
+              <a className={styles.link} href="#">
+                21 99999-9999
+                <img src={whatsapp} alt="" />
+              </a>
+              <img className={styles.icone} src={instagram} alt="" />
+              <img className={styles.icone} src={facebook} alt="" />
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
