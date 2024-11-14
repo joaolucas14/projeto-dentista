1fa51dcfae4e2778948643a98362dac901607439
joaolucas14 <jlparceiro10@gmail.com>
2024-11-14 10:34:06 -0300

    iniciando o container banner

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Banner.module.css";
+
+export default function Banner() {
+  return (
+    <div className={styles.container}>
+      <h1>Clínica Odontológica na Tijuca</h1>
+    </div>
+  );
+}
