a812cb5ca6765d7c841d6d796ee1b549e8edcee8
joaolucas14 <jlparceiro10@gmail.com>
2024-11-15 15:00:53 -0300

    criando o banner e o formulário

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -1,9 +1,18 @@
 import styles from "./Banner.module.css";
+import imagemDonos from "../../../public/imagens/donos.png";
+import Formulario from "../Formulario/index";
 
 export default function Banner() {
   return (
-    <div className={styles.container}>
-      <h1>Clínica Odontológica na Tijuca</h1>
+    <div className={styles.main}>
+      <div className={styles.titulo}>
+        <h1>Clínica Odontológica na Tijuca</h1>
+      </div>
+
+      <div className={styles.container}>
+        <img className={styles.donos} src={imagemDonos} alt="" />
+        <Formulario />
+      </div>
     </div>
   );
 }
