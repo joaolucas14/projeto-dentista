1cea699e3713ebcdfc7a0d27359dc9e79c002dc8
joaolucas14 <jlparceiro10@gmail.com>
2024-11-16 12:38:21 -0300

    começando a implementação das variaveis scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Menu from "./componentes/Menu";
 import Banner from "./componentes/Banner";
+import FaixaTexto from "./componentes/FaixaTexto";
 
 function App() {
   return (
     <>
       <Menu />
       <Banner />
+      <FaixaTexto />
     </>
   );
 }
---
+++ src/componentes/FaixaTexto/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./FaixaTexto.module.scss";
+
+export default function FaixaTexto() {
+  return (
+    <>
+      <h1 className={styles.titulo}>
+        Sua Clínica Odontológica Especializada Reabilitação Oral
+      </h1>
+      <h2>Elevando sua beleza e elevando sua confiança</h2>
+    </>
+  );
+}
