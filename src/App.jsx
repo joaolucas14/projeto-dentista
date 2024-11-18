b1f2c4ffa52aa792724e542bb44cb74ea3fee251
joaolucas14 <jlparceiro10@gmail.com>
2024-11-18 13:56:05 -0300

    iniciando o componente da clinica

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import FaixaTexto from "./componentes/FaixaTexto";
 import Vantagens from "./componentes/Vantagens";
 import Especialidade from "./componentes/Especialidades";
 import Procedimentos from "./componentes/Procedimentos";
+import Clinica from "./componentes/Clinica";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Vantagens />
       <Especialidade />
       <Procedimentos />
+      <Clinica />
     </>
   );
 }
---
+++ src/componentes/Clinica/Carrosel/index.jsx
@@ -0,0 +1,5 @@
+import styles from "./Carrosel.module.scss";
+
+export default function Carrosel() {
+  return <h1 className={styles.carrosel}>Carrosel</h1>;
+}
